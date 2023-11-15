b3abad2ff162e18217d213735d229bcabf476b05
Carsten Riedel <carstenriedel@outlook.com>
2023-11-15 20:53:23 +0100

    Commit before Release build

---
+++ src/Package/Root/content/NetTool/NetTool.MSTest/FunctionalTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace NetTool.MSTest
+{
+    [TestClass]
+    public class FunctionalTests
+    {
+
+        [TestInitialize()]
+        public void Startup()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestFooMethod()
+        {
+            var result = ClassLibrary.Class1.Foo();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result, "123");
+        }
+    }
+}

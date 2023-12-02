de1e6cd65941896faf75134669ef5d4c7232caa5
Carsten Riedel <carstenriedel@outlook.com>
2023-12-02 16:17:01 +0100

    init consoleapp

---
+++ src/Package/Root/content/ConsoleApp/ConsoleApp.Library/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp.Library
+{
+    public static class Class1
+    {
+        public static string Foo()
+        {
+            return "123";
+        }
+    }
+}
+
---
+++ src/Package/Root/content/ConsoleApp/ConsoleApp/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp
+{
+    public static class Class1
+    {
+        public static string Foo()
+        {
+            return "123";
+        }
+    }
+}
+

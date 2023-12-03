223ded7627e73d4d316f9ca79e332eb25cf4f905
Carsten Riedel <carstenriedel@outlook.com>
2023-12-03 10:46:02 +0100

    working local tested

---
+++ src/Package/Root/content/ConsoleApp/ConsoleApp.MSTest/FunctionalTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp.MSTest
+{
+    [TestClass]
+    public class FunctionalTests
+    {
+
+        [TestInitialize()]
+        public void Startup()
+        {
+
+        }
+
+        [TestMethod]
+        public void ConsoleProgramMainTest()
+        {
+            StringWriter @out = new StringWriter();
+            Console.SetOut(@out);
+
+            //Prepare answers for Console.Readline
+            string[] inputs = new string[] {
+                "Somebody",
+            };
+            //Set answers for Console.Readline
+            Console.SetIn(new StringReader(String.Join(Environment.NewLine, inputs)));
+
+            var param = new string[] { "parameter" };
+            Program.Main(param);
+
+            string? output = @out.ToString();
+
+            string expectedOutput = String.Join(Environment.NewLine, new string[] {
+                $@"Hello, World! {param[0]} {inputs[0]}",
+            }) + Environment.NewLine;
+
+            Assert.AreEqual(output, expectedOutput); 
+        }
+    }
+}
+
---
+++ src/Package/Root/content/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            string arg = String.Empty;
+            if (args.Length > 0)
+            {
+                arg = args[0];
+            }
+            Console.WriteLine($@"Hello, World! {arg} {name}");
+        }
+    }
+}
+

74435f5ac493737699ae9707079190c63f8f8109
Carsten Riedel <carstenriedel@outlook.com>
2023-12-04 09:55:03 +0100

    classlibrary docfx update

--- src/Package/Root/content/ClassLibrary/ClassLibrary/Class1.cs
+++ src/Package/Root/content/ClassLibrary/ClassLibrary/Class1.cs
@@ -5,8 +5,17 @@ using System.IO;
 
 namespace ClassLibrary
 {
+    /// <summary>
+    /// The 'Class1' provides foundational functionalities for the library.
+    /// It is a static class, meaning it cannot be instantiated and its members are accessed at the class level.
+    /// </summary>
     public static class Class1
     {
+        /// <summary>
+        /// 'Foo' is a simple method that returns a fixed string value. 
+        /// This method can be used to demonstrate basic class functionality or for testing purposes.
+        /// </summary>
+        /// <returns>Returns a string "123".</returns>
         public static string Foo()
         {
             return "123";

fda9272da523796a600101930aa8fb1bd43841ea
Carsten Riedel <carstenriedel@outlook.com>
2023-12-09 13:46:58 +0100

    update

--- raw/dotnet.bat
+++ raw/dotnet.bat
@@ -3,6 +3,7 @@ cls
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -channel 6.0"
 cd %userprofile%\AppData\Local\Microsoft\dotnet
 dotnet tool install --global Coree.Dotnet.VsVsix --version 0.1.0
+dotnet-vs-vsix
 REM echo $*
 REM echo %*
 REM powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -channel 6.0"

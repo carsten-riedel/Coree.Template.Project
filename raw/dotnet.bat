1a172ddd13d3c1b0c5c85024a748fea5e8743678
Carsten Riedel <carstenriedel@outlook.com>
2023-12-09 13:45:58 +0100

    update

--- raw/dotnet.bat
+++ raw/dotnet.bat
@@ -1,5 +1,8 @@
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Runtime dotnet -channel 6.0"
-cd %userprofile%
+@echo off
+cls
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -channel 6.0"
+cd %userprofile%\AppData\Local\Microsoft\dotnet
+dotnet tool install --global Coree.Dotnet.VsVsix --version 0.1.0
 REM echo $*
 REM echo %*
 REM powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -channel 6.0"

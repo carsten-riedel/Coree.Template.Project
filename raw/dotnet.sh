a0224a4e103a868053a6935f53fafa180ce50548
Carsten Riedel <carstenriedel@outlook.com>
2023-12-11 07:56:07 +0100

    update

--- raw/dotnet.sh
+++ raw/dotnet.sh
@@ -2,6 +2,23 @@
 # sudo curl -sSL https://raw.githubusercontent.com/carsten-riedel/Coree.Template.Project/main/raw/dotnet.sh | bash
 clear
 
+addToBashrc() {
+    local exportLine=$1
+    if [ -z "$exportLine" ]; then
+        echo "No export line provided."
+        return 1
+    fi
+
+    if ! grep -qF -- "$exportLine" "$HOME/.bashrc"; then
+        echo "$exportLine" >> "$HOME/.bashrc"
+        echo "Line added to .bashrc"
+    else
+        echo "Line already present in .bashrc"
+    fi
+}
+
+
+
 # Update the system
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -17,12 +34,12 @@ export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
 echo 'export DOTNET_ROOT=$HOME/.dotnet' >> $HOME/.bashrc
 echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> $HOME/.bashrc
 
-
-dotnet tool install --global PowerShell
+dotnet tool install --global PowerShell 2>nul
 
 wget --content-disposition -O code.deb https://go.microsoft.com/fwlink/?LinkID=760868 && sudo apt install -y ./code.deb && rm -f ./code.deb
 
 export DONT_PROMPT_WSL_INSTALL=1 ;
-echo 'export DONT_PROMPT_WSL_INSTALL=1' >> $HOME/.bashrc ;
+
+addToBashrc "export DONT_PROMPT_WSL_INSTALL=1"
 mkdir -p "$HOME/source/repos"
 mkdir -p "$HOME/source/packages"

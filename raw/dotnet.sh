85fb9e07adfea22a5e3420b2db35d94fa8abc35f
Carsten Riedel <carstenriedel@outlook.com>
2023-12-11 07:47:18 +0100

    update

--- raw/dotnet.sh
+++ raw/dotnet.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 # sudo curl -sSL https://raw.githubusercontent.com/carsten-riedel/Coree.Template.Project/main/raw/dotnet.sh | bash
 clear
-sudo apt-get update && sudo apt-get -y upgrade
+
+# Update the system
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# Install dotnet
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 6.0
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 7.0
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -channel 8.0
-export DOTNET_ROOT=$HOME/.dotnet ; export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools ; echo 'export DOTNET_ROOT=$HOME/.dotnet' >> $HOME/.bashrc && echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> $HOME/.bashrc
+
+
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> $HOME/.bashrc
+echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> $HOME/.bashrc
+
+
 dotnet tool install --global PowerShell
-sudo wget --content-disposition -O code.deb https://go.microsoft.com/fwlink/?LinkID=760868 && sudo apt install -y ./code.deb && rm -f ./code.deb
-export DONT_PROMPT_WSL_INSTALL=1 ; echo 'export DONT_PROMPT_WSL_INSTALL=1' >> $HOME/.bashrc ; mkdir -p "$HOME/source/repos" ; mkdir -p "$HOME/source/packages"
+
+wget --content-disposition -O code.deb https://go.microsoft.com/fwlink/?LinkID=760868 && sudo apt install -y ./code.deb && rm -f ./code.deb
+
+export DONT_PROMPT_WSL_INSTALL=1 ;
+echo 'export DONT_PROMPT_WSL_INSTALL=1' >> $HOME/.bashrc ;
+mkdir -p "$HOME/source/repos"
+mkdir -p "$HOME/source/packages"

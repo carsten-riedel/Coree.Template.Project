d105952f2d003678faaeea906dffba5c320541c5
Carsten Riedel <carstenriedel@outlook.com>
2023-12-12 22:02:12 +0100

    update jenkins

--- raw/dotnet.sh
+++ raw/dotnet.sh
@@ -56,11 +56,40 @@ dotnet tool install --global PowerShell 2>nul
 
 result=$(doesCommandExist code)
 if [ "$result" != "true" ]; then
-    wget --no-clobber --content-disposition -O code.deb https://go.microsoft.com/fwlink/?LinkID=760868 && sudo apt install -y ./code.deb && rm -f ./code.deb   
+    wget --no-clobber --content-disposition -O code.deb https://go.microsoft.com/fwlink/?LinkID=760868
+    sudo apt install -y ./code.deb
+    rm -f ./code.deb
+    export DONT_PROMPT_WSL_INSTALL=1 ;
+    addLineToFile "export DONT_PROMPT_WSL_INSTALL=1" "$HOME/.bashrc" 
 fi
-
-export DONT_PROMPT_WSL_INSTALL=1 ;
-addLineToFile "export DONT_PROMPT_WSL_INSTALL=1" "$HOME/.bashrc" 
 
 mkdir -p "$HOME/source/repos"
 mkdir -p "$HOME/source/packages"
+
+# Chrome
+resultChrome=$(doesCommandExist chrome)
+if [ "$resultChrome" != "true" ]; then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt -y install ./google-chrome-stable_current_amd64.deb
+    rm -f ./google-chrome-stable_current_amd64.deb
+    addLineToFile "alias chrome='setsid google-chrome &>/dev/null'" "~/.bash_aliases"
+fi
+
+resultNginx=$(doesCommandExist nginx)
+if [ "$resultChrome" != "true" ]; then
+    sudo apt-get install nginx
+    sudo chmod -v -R 777 /var/www/html
+fi
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt install fontconfig openjdk-17-jre
+sudo apt-get install jenkins
+
+#sudo systemctl edit jenkins /etc/systemd/system/jenkins.service.d/override.conf
+#[Service]
+#Environment="JENKINS_PORT=8181"
+#Environment="JAVA_OPTS=-Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true"
+#check http://[::1]
+

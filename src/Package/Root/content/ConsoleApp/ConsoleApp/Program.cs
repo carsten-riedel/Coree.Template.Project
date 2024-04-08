e69579a51c7c7bef66318da59e4e85d5269aed2f
Carsten Riedel <carstenriedel@outlook.com>
2024-04-08 07:22:48 +0200

    update

--- src/Package/Root/content/ConsoleApp/ConsoleApp/Program.cs
+++ src/Package/Root/content/ConsoleApp/ConsoleApp/Program.cs
@@ -9,13 +9,24 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
+            // Output a prompt message to the console instructing the user to enter a value.
+            Console.WriteLine("Please enter a value:");
+
+            // Read the user's input from the console and store it in the 'name' variable.
             var name = Console.ReadLine();
-            string arg = String.Empty;
+
+            // Initialize a variable to hold a command-line argument if provided.
+            string arg = string.Empty;
+
+            // Check if any command-line arguments were provided.
             if (args.Length > 0)
             {
+                // Assign the first command-line argument to the 'arg' variable.
                 arg = args[0];
             }
-            Console.WriteLine($@"Hello, World! {arg} {name} {ConsoleApp.Library.Class1.Foo()}");
+
+            // Output a greeting message to the console, incorporating the command-line argument, user input, and result of the Foo() method call.
+            Console.WriteLine($"Hello, World! {arg} {name} {ConsoleApp.Library.Class1.Foo()}");
         }
     }
 }
